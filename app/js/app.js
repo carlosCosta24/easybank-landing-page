3aa8569bd86d8b98b99b347584ab35b1400dc4bd
M0lt0 <bodymgahed@yahoo.com>
2024-04-23 23:36:18 -0300

    drop-down menu logic and style

--- app/js/app.js
+++ app/js/app.js
@@ -1,39 +1,29 @@
+const body = document.querySelector('body')
 const menuButton = document.getElementById("open");
 const headerMenu = document.getElementsByClassName("header__menu")[0];
 const heder = document.getElementsByClassName("header")[0];
 const dropDownMenu = document.getElementsByClassName("dropdown")[0];
+
 menuButton.addEventListener("click", function changeStatus() {
     if (heder.classList.contains("open")) {
         heder.classList.remove("open");
         dropDownMenu.classList.remove("fade-in");
         dropDownMenu.classList.add("fade-out");
-        headerMenu.style.visibility = "hidden"
+        window.removeEventListener('scroll', prevent);
+
     } else {
         heder.classList.add("open");
         headerMenu.style.visibility = "visible"
         dropDownMenu.classList.add("fade-in");
         dropDownMenu.classList.remove("fade-out");
+        window.addEventListener('scroll', prevent);
     }
+
+
 });
-// dropDownMenu.addEventListener('click', () => {
-//     if ("fade-in") {
-//         heder.classList.remove("open");
-//         dropDownMenu.classList.remove("fade-in");
-//         headerMenu.style.visibility = "hidden";
-//         dropDownMenu.remove();
-//         dropDownMenu.classList.add("fade-out");
-//     }
-// });
-
-// window.addEventListener("scroll", () => {
-//     if ("fade-in") {
-//         heder.classList.remove("open");
-//         dropDownMenu.classList.remove("fade-in");
-//         headerMenu.style.visibility = "hidden";
-//         dropDownMenu.classList.add("fade-out");
-//     }
-// })
-
+const prevent = () => {
+    window.scrollTo(0, 0)
+}
 //hover effect on social links
 const img = document.querySelectorAll('.footer__logo')
 console.log(img)
